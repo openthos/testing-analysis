8aa8c50450676d3c3cb72167cd0408006a9bff97
eva980636 <147300786@qq.com>
2016-05-25 16:00:07 +0800

    add uiautomator

---
+++ Auto-test/uiautomator/demo1/bin/classes.dex.d
@@ -0,0 +1,3 @@
+/home/emindsoft/workspace/demo1/bin/classes.dex : \
+/home/emindsoft/workspace/demo1/bin/classes/com/browser/UiAutomatorHelper.class \
+/home/emindsoft/workspace/demo1/bin/classes/com/browser/demobrowser.class \
---
+++ Auto-test/uiautomator/emaildemo/bin/classes.dex.d
@@ -0,0 +1,2 @@
+/home/emindsoft/workspace/emaildemo/bin/classes.dex : \
+/home/emindsoft/workspace/emaildemo/bin/classes/com/email/emailset.class \
