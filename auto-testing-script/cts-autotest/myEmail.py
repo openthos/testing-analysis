415195395882296e55de7502c01c5350519253e0
aoquan <qaoimu@163.com>
2016-09-07 17:08:32 +0800

    makes it work with docker compiling

---
+++ auto-testing-script/cts-autotest/myEmail.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+import smtplib  
+from email.mime.text import MIMEText
+from email.header import Header
+
+mailto_list=["qaoimu@163.com"] 
+mail_host="smtp.163.com"  #设置服务器
+mail_user="cscwlab"    #用户名
+mail_pass="cscw9410"   #口令 
+mail_postfix="163.com"  #发件箱的后缀
+  
+def send_mail(to_list,sub,content):  
+    me="hello"+"<"+mail_user+"@"+mail_postfix+">"  
+    msg = MIMEText(content,_subtype='plain',_charset='gb2312')  
+    msg['Subject'] = sub  
+    msg['From'] = me  
+    msg['To'] = ";".join(to_list)  
+    try:  
+        server = smtplib.SMTP()  
+        server.connect(mail_host)  
+        server.login(mail_user,mail_pass)  
+        server.sendmail(me, to_list, msg.as_string())  
+        server.close()  
+        return True  
+    except Exception, e:  
+        print str(e)  
+        return False  
+if __name__ == '__main__':  
+    if send_mail(mailto_list,"test python mail","qaoimu, it's email sent by system, do not reply"):  
+        print "发送成功"  
+    else:  
+        print "发送失败"
