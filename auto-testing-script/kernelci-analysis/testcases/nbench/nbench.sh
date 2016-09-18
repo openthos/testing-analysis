8ad426a727c00bbc4e32a62098c2a1557ca3b377
aoquan <qaoimu@163.com>
2016-09-18 16:22:12 +0800

    add all testcase

--- auto-testing-script/kernelci-analysis/testcases/nbench/nbench.sh
+++ auto-testing-script/kernelci-analysis/testcases/nbench/nbench.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./nbench
+androidIP=$1
+port=$2
+commitId=$3
+cd "$(dirname "$0")"
+adb -s $androidIP:$port push nbench /data/local/tmp
+adb -s $androidIP:$port shell /data/local/tmp/nbench
