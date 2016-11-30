2469ee31c9e314b32e32e2e3a9e17adfe4c128f1
mao_ying_ming <elwin_mao@163.com>
2016-11-30 17:23:55 +0800

    Update creat_symbol.sh

--- lkp_result_web/creat_symbol.sh
+++ lkp_result_web/creat_symbol.sh
@@ -3,6 +3,7 @@ apt-get install -y  apache2
 apt-get install -y  php5
 apt-get install -y  libapache2-mod-php5
 
+cp ./* -R  /var/www/html
 cd /var/www/html
 ln -s /mnt/freenas/result/ .
 ln -s /mnt/freenas/summary/ .
