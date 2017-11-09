e4e81aefd95906be234ed4dbd04b78fb470dfd9e
jiangqin0501 <hecateqin@sina.com>
2017-11-09 13:19:48 +0800

    modify autoTest.sh to avoid run startmenu test twice

--- auto-testing-script/cts-autotest/autoTest.sh
+++ auto-testing-script/cts-autotest/autoTest.sh
@@ -404,15 +404,6 @@ elif [ "$r_v" == "r" ];then
         adb -s $ip_android:$adbPort shell pm install -r "/data/local/tmp/com.example.qin.startmenutest.test"
         adb -s $ip_android:$adbPort shell am instrument -w -r   -e debug false -e class com.example.qin.startmenutest.StartMenuTest1 com.example.qin.startmenutest.test/android.support.test.runner.AndroidJUnitRunner
         adb -s $ip_android:$adbPort shell pm uninstall com.example.qin.startmenutest
-
-	##startmenuTest
-        echo 'startmenutest'
-        adb -s $ip_android:$adbPort push bin/StartMenuTest.apk /data/local/tmp/com.example.qin.startmenutest || { echo "push StartMenuTest.apk failed" ; exit 1 ; }
-        adb -s $ip_android:$adbPort shell pm install -r "/data/local/tmp/com.example.qin.startmenutest"
-        adb -s $ip_android:$adbPort push bin/StartMenuTest-androidTest.apk /data/local/tmp/com.example.qin.startmenutest.test || { echo "push StartMenuTest-androidTest.apk failed" ; exit 1 ; }
-        adb -s $ip_android:$adbPort shell pm install -r "/data/local/tmp/com.example.qin.startmenutest.test"
-        adb -s $ip_android:$adbPort shell am instrument -w -r   -e debug false -e class com.example.qin.startmenutest.StartMenuTest1 com.example.qin.startmenutest.test/android.support.test.runner.AndroidJUnitRunner
-        adb -s $ip_android:$adbPort shell pm uninstall com.example.qin.startmenutest
         
 	echo 'install CtsDeviceAdmin.apk!!!!!'
         adb -s $ip_android:$adbPort install $testcaseCTS/repository/testcases/CtsDeviceAdmin.apk
