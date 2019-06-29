7b887bb9c985c015e7e406f2ffeeed19c44b979c
jasmqin <hecateqin@sina.com>
2019-06-29 19:08:11 +0800

    更新多窗口测试用例

--- Auto-test/python+uiautomator2/windowtest.py
+++ Auto-test/python+uiautomator2/windowtest.py
@@ -121,6 +121,78 @@ class WindowTestCase(unittest.TestCase):
         windowBounds = resize("right-bottom","plus")
         windowBounds = resize("right-bottom","minus")
 
+    def test_11_move_left1(self):
+        logger.info("------------标准窗口测试：移动-屏幕内左移")
+        caption_x,caption_y = d(resourceId="android:id/caption").center()
+        windowBounds=extendfunction.getParentBounds(d.dump_hierarchy(),'resource-id="android:id/caption"')
+        window_width=windowBounds['right']-windowBounds['left']
+        window_height=windowBounds['top']-windowBounds['bottom']
+        d.drag(caption_x,caption_y,window_width/2+100,caption_y,0.1)
+        check_windowBounds = {'top': windowBounds['top'], 'bottom': windowBounds['bottom'], 'left': 100,'right':100+window_width}
+        windowBounds=extendfunction.getParentBounds(d.dump_hierarchy(),'resource-id="android:id/caption"')
+        print(check_windowBounds)
+        print(windowBounds)
+        output_res(windowBounds==check_windowBounds)
+
+    def test_12_move_right1(self):
+        logger.info("------------标准窗口测试：移动-屏幕内右移")
+        caption_x,caption_y = d(resourceId="android:id/caption").center()
+        windowBounds=extendfunction.getParentBounds(d.dump_hierarchy(),'resource-id="android:id/caption"')
+        window_width=windowBounds['right']-windowBounds['left']
+        window_height=windowBounds['top']-windowBounds['bottom']
+        d.drag(caption_x,caption_y,1600-100-window_width/2,caption_y,0.1)
+        check_windowBounds = {'top': windowBounds['top'], 'bottom': windowBounds['bottom'], 'left': 1600-100-window_width,'right':1600-100}
+        windowBounds=extendfunction.getParentBounds(d.dump_hierarchy(),'resource-id="android:id/caption"')
+        print(check_windowBounds)
+        print(windowBounds)
+        output_res(windowBounds==check_windowBounds)
+
+    def test_13_move_up1(self):
+        logger.info("------------标准窗口测试：移动-屏幕内上移")
+
+    def test_14_move_down1(self):
+        logger.info("------------标准窗口测试：移动-屏幕内下移")
+
+    def test_15_move_left2(self):
+        logger.info("------------标准窗口测试：移动-出屏幕左移")
+
+    def test_16_move_right2(self):
+        logger.info("------------标准窗口测试：移动-出屏幕右移")
+
+    def test_17_move_leftdown(self):
+        logger.info("------------标准窗口测试：移动-出屏幕左下")
+
+    def test_18_move_rightdown(self):
+        logger.info("------------标准窗口测试：移动-出屏幕右下")
+
+    def test_19_move_top(self):
+        logger.info("------------标准窗口测试：移动-上边界")
+
+    def test_20_move_leftside(self):
+        logger.info("------------标准窗口测试：移动-左边界")
+
+    def test_21_move_rightside(self):
+        logger.info("------------标准窗口测试：移动-右边界")
+
+    def test_22_doubleclick(self):
+        logger.info("------------标准窗口测试：双击")
+
+    def test_31_button_minimum(self):
+        logger.info("------------标准窗口测试：按钮-最小化")
+
+    def test_32_button_max(self):
+        logger.info("------------标准窗口测试：按钮-最大化")
+
+    def test_33_button_close(self):
+        logger.info("------------标准窗口测试：按钮-关闭")
+
+    def test_34_button_setting(self):
+        logger.info("------------标准窗口测试：按钮-设置")
+
+    def test_51_fullscreen_button(self):
+        logger.info("------------全屏窗口测试：按钮")
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         WindowTestCase.package = sys.argv.pop()
@@ -129,4 +201,4 @@ if __name__ == "__main__":
     sleep(2)
     test_file.close()
     d.service("uiautomator").stop()
-
+x, y = d(text="Settings").center()
